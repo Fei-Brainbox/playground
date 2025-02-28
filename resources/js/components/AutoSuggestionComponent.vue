bc667e61ca81461f7ec36fff41d9189aef783a55
Fei <fei@brainboxlabs.com>
2025-02-28 15:53:29 -0500

    add pre set code

--- resources/js/components/AutoSuggestionComponent.vue
+++ resources/js/components/AutoSuggestionComponent.vue
@@ -4,17 +4,18 @@
             v-model="value" 
             mode="tags" 
             :options=options 
-            :close-on-select="false"
+            :close-on-select="false" 
             :create-option="true"
-            :searchable="true"
-            :max=3
-            :placeholder="placeholder"
-            ref="myMultiSelect"
-            @select="checkGroup()"
-        />
+            :searchable="true" 
+            :max=3 
+            :placeholder="placeholder" 
+            ref="myMultiSelect" 
+            @select="checkGroup()" />
 
         <span class="pl-4" v-if="reachMaximum">{{ this.custom_message }}</span>
-        
+        <pre>
+            {{ value }}
+        </pre>
     </div>
 </template>
 
@@ -27,41 +28,56 @@ export default {
     },
     data() {
         return {
-            value: null,
-            group: [],
+            value: [],
             options: [],
             reachMaximum: false,
             custom_message: 'You can only add up to 3 groups.',
-            placeholder:'Start typing...',
+            placeholder: 'Start typing...',
+            exists: 'EozNljK',
         }
     },
 
     mounted() {
         axios.get('https://cblq.brainbox.test/api/groups')
             .then(response => {
-               this.group = response.data.payload.data;
-               this.group.forEach(element => {
-                    const item = {};
-                    item.value = element.code;
-                    item.label = element.name;
-                    item.id = element.id;
-
-                    this.options.push(item)
-               });
-            }).catch(()=>{
+                this.options = (response.data.payload.data).map(item => {
+                    return {
+                        text: item.name,
+                        value: item,
+                        label: item.name,
+                        ...item
+                    }
+                });
+                if (this.exists && this.groupExistsInList(this.exists)) {
+                    
+                    this.value.push(this.options.find(item => item.code == this.exists || item.legacy_code == this.exists))
+                    console.log(this.value)
+                }
+            }).catch(() => {
                 console.log('errors');
-            })
+            });
+        
+        
+
     },
 
-    methods:{
+    methods: {
         checkGroup() {
+            console.log(this.value)
             this.reachMaximum = this.value.length >= 3 ? true : false;
+            
+            if (this.reachMaximum) this.$refs.myMultiSelect.close();
+        },
 
-            if(this.reachMaximum) this.$refs.myMultiSelect.close();
-        }
+        groupExistsInList(group_code) {
+            if (group_code == null) {
+                return false;
+            }
+            return this.options.some(item => item.code == group_code || item.legacy_code == group_code);
+        },
+
     }
 }
 </script>
 
-<style src="@vueform/multiselect/themes/default.css">
-</style>
+<style src="@vueform/multiselect/themes/default.css"></style>
